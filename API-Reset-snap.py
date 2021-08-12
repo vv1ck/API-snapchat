03bddd52614158b5ca5e53116045176fc939bee3
vv1ck <75469718+vv1ck@users.noreply.github.com>
2021-08-12 04:13:07 +0300

    Create API-Reset-snap.py

---
+++ API-Reset-snap.py
@@ -0,0 +1,85 @@
+import requests,re
+from user_agent import generate_user_agent
+r=requests.session()
+URL1="https://www.google.com/recaptcha/enterprise/anchor?ar=1&k=6LezjdAZAAAAAD1FaW81QpkkplPNzCNnIOU5anHw&co=aHR0cHM6Ly9hY2NvdW50cy5zbmFwY2hhdC5jb206NDQz&hl=ar&v=CdDdhZfPbLLrfYLBdThNS0-Y&size=invisible&badge=inline&cb=3b4l61jprpqj"
+
+
+user = 'emqil@email.com'
+
+
+
+def Reset_the_password():
+	global Done,Heda,Done2
+	
+	url = 'https://accounts.snapchat.com/accounts/password_reset_request'
+	
+	TOK ="".join(re.findall('data-xsrf="(.*?)"', str(r.get(url, headers=Heda).text)))
+	
+	
+	headers = {
+	'Host': 'accounts.snapchat.com',
+	'Cookie': 'xsrf_token='+TOK+'; Marketing=true; Performance=true; Preferences=true; sc-cookies-accepted=true; _sca={%22cid%22:%22c31544c3-a7dc-40d0-923f-e8bab6ebe5bd%22%2C%22token%22:%22v1.key.2021-03-06_YAkgxx20.iv.FZp7AeoFubjeJxKK.o2OgkonFpzLIHOH/PIdVfxUzMq5qL1p9dGcO3DLkqgDEFg2ZjPsR5WTy7F2EiGRiCzSAk4PZMPpeoC7vPtzjpJ2mj6iKo6T/twHnFX/W/mv9Em7I%22}; web_client_id=f93a4909-8774-43d6-9a52-2b2169795ec8',
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Content-Type': 'application/x-www-form-urlencoded',
+	'Origin': 'https://accounts.snapchat.com',
+	'Content-Length': '3557',
+	'Accept-Language': 'ar',
+	'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148',
+	'Referer': 'https://accounts.snapchat.com/',
+	'Accept-Encoding': 'gzip, deflate',
+	'Connection': 'close' }
+	
+	
+	data = {
+		'emailaddress': user,
+		'xsrf_token': TOK,
+		'g-recaptcha-response': Done2,
+		'g-recaptcha-response': Done2}
+	
+	Go = r.post(url,headers=headers,data=data)
+	
+	print(Go)
+	print(Go.text)
+
+
+def captcha():
+  global Done,Heda,Done2
+  URL2 = 'https://www.google.com/recaptcha/enterprise/reload?k=6LezjdAZAAAAAD1FaW81QpkkplPNzCNnIOU5anHw'
+  HEDsp = {
+    "Accept": "*/*",
+    "accept-encoding": "gzip, deflate, br",
+    "accept-language": "fa,en;q=0.9,en-GB;q=0.8,en-US;q=0.7",
+    "content-length": "5628",
+    "origin": "https://www.google.com",
+    "User-Agent": generate_user_agent(),
+    "Pragma": "no-cache",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-origin",
+    "referer": "https://www.google.com/recaptcha/enterprise/anchor?ar=1&k=6LezjdAZAAAAAD1FaW81QpkkplPNzCNnIOU5anHw&co=aHR0cHM6Ly9hY2NvdW50cy5zbmFwY2hhdC5jb206NDQz&hl=ar&v=CdDdhZfPbLLrfYLBdThNS0-Y&size=invisible&badge=inline&cb=3b4l61jprpqj"}
+  DATsp = {
+    "v":"CdDdhZfPbLLrfYLBdThNS0-Y",	
+    "reason":"q",
+    "c":Done,
+    "k":"",
+    "co":"",
+    "hl":"en",
+    "size":"invisible",
+    "chr":"%5B89%2C64%2C27%5D",
+    "vh":"13599012192",
+    "bg":"!q62grYxHRvVxjUIjSFNd0mlvrZ-iCgIHAAAB6FcAAAANnAkBySdqTJGFRK7SirleWAwPVhv9-XwP8ugGSTJJgQ46-0IMBKN8HUnfPqm4sCefwxOOEURND35prc9DJYG0pbmg_jD18qC0c-lQzuPsOtUhHTtfv3--SVCcRvJWZ0V3cia65HGfUys0e1K-IZoArlxM9qZfUMXJKAFuWqZiBn-Qi8VnDqI2rRnAQcIB8Wra6xWzmFbRR2NZqF7lDPKZ0_SZBEc99_49j07ISW4X65sMHL139EARIOipdsj5js5JyM19a2TCZJtAu4XL1h0ZLfomM8KDHkcl_b0L-jW9cvAe2K2uQXKRPzruAvtjdhMdODzVWU5VawKhpmi2NCKAiCRUlJW5lToYkR_X-07AqFLY6qi4ZbJ_sSrD7fCNNYFKmLfAaxPwPmp5Dgei7KKvEQmeUEZwTQAS1p2gaBmt6SCOgId3QBfF_robIkJMcXFzj7R0G-s8rwGUSc8EQzT_DCe9SZsJyobu3Ps0-YK-W3MPWk6a69o618zPSIIQtSCor9w_oUYTLiptaBAEY03NWINhc1mmiYu2Yz5apkW_KbAp3HD3G0bhzcCIYZOGZxyJ44HdGsCJ-7ZFTcEAUST-aLbS-YN1AyuC7ClFO86CMICVDg6aIDyCJyIcaJXiN-bN5xQD_NixaXatJy9Mx1XEnU4Q7E_KISDJfKUhDktK5LMqBJa-x1EIOcY99E-eyry7crf3-Hax3Uj-e-euzRwLxn2VB1Uki8nqJQVYUgcjlVXQhj1X7tx4jzUb0yB1TPU9uMBtZLRvMCRKvFdnn77HgYs5bwOo2mRECiFButgigKXaaJup6NM4KRUevhaDtnD6aJ8ZWQZTXz_OJ74a_OvPK9eD1_5pTG2tUyYNSyz-alhvHdMt5_MAdI3op4ZmcvBQBV9VC2JLjphDuTW8eW_nuK9hN17zin6vjEL8YIm_MekB_dIUK3T1Nbyqmyzigy-Lg8tRL6jSinzdwOTc9hS5SCsPjMeiblc65aJC8AKmA5i80f-6Eg4BT305UeXKI3QwhI3ZJyyQAJTata41FoOXl3EF9Pyy8diYFK2G-CS8lxEpV7jcRYduz4tEPeCpBxU4O_KtM2iv4STkwO4Z_-c-fMLlYu9H7jiFnk6Yh8XlPE__3q0FHIBFf15zVSZ3qroshYiHBMxM5BVQBOExbjoEdYKx4-m9c23K3suA2sCkxHytptG-6yhHJR3EyWwSRTY7OpX_yvhbFri0vgchw7U6ujyoXeCXS9N4oOoGYpS5OyFyRPLxJH7yjXOG2Play5HJ91LL6J6qg1iY8MIq9XQtiVZHadVpZVlz3iKcX4vXcQ3rv_qQwhntObGXPAGJWEel5OiJ1App7mWy961q3mPg9aDEp9VLKU5yDDw1xf6tOFMwg2Q-PNDaKXAyP_FOkxOjnu8dPhuKGut6cJr449BKDwbnA9BOomcVSztEzHGU6HPXXyNdZbfA6D12f5lWxX2B_pobw3a1gFLnO6mWaNRuK1zfzZcfGTYMATf6d7sj9RcKNS230XPHWGaMlLmNxsgXkEN7a9PwsSVwcKdHg_HU4vYdRX6vkEauOIwVPs4dS7yZXmtvbDaX1zOU4ZYWg0T42sT3nIIl9M2EeFS5Rqms_YzNp8J-YtRz1h5RhtTTNcA5jX4N-xDEVx-vD36bZVzfoMSL2k85PKv7pQGLH-0a3DsR0pePCTBWNORK0g_RZCU_H898-nT1syGzNKWGoPCstWPRvpL9cnHRPM1ZKemRn0nPVm9Bgo0ksuUijgXc5yyrf5K49UU2J5JgFYpSp7aMGOUb1ibrj2sr-D63d61DtzFJ2mwrLm_KHBiN_ECpVhDsRvHe5iOx_APHtImevOUxghtkj-8RJruPgkTVaML2MEDOdL_UYaldeo-5ckZo3VHss7IpLArGOMTEd0bSH8tA8CL8RLQQeSokOMZ79Haxj8yE0EAVZ-k9-O72mmu5I0wH5IPgapNvExeX6O1l3mC4MqLhKPdOZOnTiEBlSrV4ZDH_9fhLUahe5ocZXvXqrud9QGNeTpZsSPeIYubeOC0sOsuqk10sWB7NP-lhifWeDob-IK1JWcgFTytVc99RkZTjUcdG9t8prPlKAagZIsDr1TiX3dy8sXKZ7d9EXQF5P_rHJ8xvmUtCWqbc3V5jL-qe8ANypwHsuva75Q6dtqoBR8vCE5xWgfwB0GzR3Xi_l7KDTsYAQIrDZVyY1UxdzWBwJCrvDrtrNsnt0S7BhBJ4ATCrW5VFPqXyXRiLxHCIv9zgo-NdBZQ4hEXXxMtbem3KgYUB1Rals1bbi8X8MsmselnHfY5LdOseyXWIR2QcrANSAypQUAhwVpsModw7HMdXgV9Uc-HwCMWafOChhBr88tOowqVHttPtwYorYrzriXNRt9LkigESMy1bEDx79CJguitwjQ9IyIEu8quEQb_-7AEXrfDzl_FKgASnnZLrAfZMtgyyddIhBpgAvgR_c8a8Nuro-RGV0aNuunVg8NjL8binz9kgmZvOS38QaP5anf2vgzJ9wC0ZKDg2Ad77dPjBCiCRtVe_dqm7FDA_cS97DkAwVfFawgce1wfWqsrjZvu4k6x3PAUH1UNzQUxVgOGUbqJsaFs3GZIMiI8O6-tZktz8i8oqpr0RjkfUhw_I2szHF3LM20_bFwhtINwg0rZxRTrg4il-_q7jDnVOTqQ7fdgHgiJHZw_OOB7JWoRW6ZlJmx3La8oV93fl1wMGNrpojSR0b6pc8SThsKCUgoY6zajWWa3CesX1ZLUtE7Pfk9eDey3stIWf2acKolZ9fU-gspeACUCN20EhGT-HvBtNBGr_xWk1zVJBgNG29olXCpF26eXNKNCCovsILNDgH06vulDUG_vR5RrGe5LsXksIoTMYsCUitLz4HEehUOd9mWCmLCl00eGRCkwr9EB557lyr7mBK2KPgJkXhNmmPSbDy6hPaQ057zfAd5s_43UBCMtI-aAs5NN4TXHd6IlLwynwc1zsYOQ6z_HARlcMpCV9ac-8eOKsaepgjOAX4YHfg3NekrxA2ynrvwk9U-gCtpxMJ4f1cVx3jExNlIX5LxE46FYIhQ"}
+  GO2 = r.post(URL2,headers=HEDsp, data=DATsp)
+  Done2 = "".join(re.findall("\[\"rresp\",\"(.*?)\"", str(GO2.text)))
+  Reset_the_password()
+def recaptcha():
+  global Done,Heda
+  Heda = {
+    "Accept": "*/*",
+    "Pragma": "no-cache",
+    "User-Agent": generate_user_agent()}
+    
+  GO1 = r.get(URL1, headers=Heda)
+  Done = "".join(re.findall("type=\"hidden\" id=\"recaptcha-token\" value=\"(.*?)\"", str(GO1.text)))
+  
+  captcha()
+recaptcha()
